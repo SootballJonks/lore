c336a8ea2e640b8905e45214dfe253e2fd0394a5
Jonks <anastasiablackhold@gmail.com>
2021-03-06 19:10:48 +0000

    Implemented barebones login functionality.

--- server.js
+++ server.js
@@ -2,32 +2,34 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
-const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+const PORT           = process.env.PORT || 8080;
+const ENV            = process.env.ENV || "development";
+const express        = require("express");
+const bodyParser     = require("body-parser");
+const sass           = require("node-sass-middleware");
+const app            = express();
+const morgan         = require('morgan');
+const cookieSession  = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
-const db = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'labber',
-  password: 'labber',
-  database: 'midterm'
-});
+const db = new Pool(dbParams);
 db.connect();
 
+
+// console.log('connected', db);
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -35,28 +37,32 @@ app.use("/styles", sass({
   debug: true,
   outputStyle: 'expanded'
 }));
-app.use(express.static("public"));
+
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const storiesRoutes = require("./routes/stories");
-const loginRoutes = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(db));
 app.use("/stories", storiesRoutes(db));
-app.use("/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
-
+app.use(express.static("public"));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get('/login/:id', (req, res) => {
+  req.session.userID = req.params.id;
+  res.redirect('/');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
