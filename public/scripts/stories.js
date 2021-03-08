3f3bcd86340f3ff3da15d08bbafc6ada95b351af
zezking <zhaoenze001@gmail.com>
2021-03-07 20:20:23 -0800

    finish click each story to show the full text

--- public/scripts/stories.js
+++ public/scripts/stories.js
@@ -7,18 +7,11 @@ const loadStories = (action) => {
 //render all the stories in the all-stories class in stories.ejs
 const renderStories = (stories) => {
   $(".all-stories").empty();
-
   for (story of stories) {
-    $(".all-stories").append(createStories(story)); //append all the stories
-  }
+    $(".all-stories").append(createStories(story));
+  } //append all the stories
 };
-const renderSingleStory = (stories) => {
-  $(".all-stories").empty();
 
-  for (story of stories) {
-    $(".all-stories").append(createStories(story)); //append all the stories
-  }
-};
 //create the story elements to append in the stories.ejs
 const createStories = (story) => {
   let snippet = "";
@@ -40,13 +33,31 @@ const createStories = (story) => {
   return $story;
 };
 
-const createSingleStory = (storyId) => {
+const renderSingleStory = () => {
+  $("main").on("click", () => {
+    let storyID = $(event.target).parent()[0].id.slice(-1);
+
+    if (storyID) {
+      $(".all-stories").empty();
+      console.log(storyID);
+      $.ajax("api/stories", { method: "get" })
+        .then((res) => RenderSingleStory(res[storyID - 1]))
+        .catch((err) => console.log(err));
+    }
+  });
+};
+
+const RenderSingleStory = (story) => {
+  $(".all-stories").append(createSingleStory(story));
+};
+
+const createSingleStory = (story) => {
   let $story = $(`<wired-card elevation="4" id="story-${story.id}" class="story">
   <header>
     <span class="story-title">${story.title}</span>
   </header>
-  <p class="story-text">${story.text}...</p>
-  <span class="story-read-more">Read More</span>
+  <p class="story-text">${story.text}</p>
+
   <footer class="story-tags">
     ${story.tags}
     </div>
@@ -56,16 +67,7 @@ const createSingleStory = (storyId) => {
   return $story;
 };
 
-const renderSingleStory = () => {
-  let allWiredCards = $("wired-card");
-
-  $("main").on("click", () => {
-    console.log($(event.target).parent(".story")[0].id);
-  });
-};
-
 $(document).ready(() => {
   renderSingleStory();
   loadStories(renderStories);
-  createSingleStory();
 });
