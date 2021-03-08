41b20c035c4a1f1ce7cf95f3d8fa1a12c935e100
zezking <zhaoenze001@gmail.com>
2021-03-07 19:16:20 -0800

    finish detect click story logic

--- public/scripts/stories.js
+++ public/scripts/stories.js
@@ -12,13 +12,20 @@ const renderStories = (stories) => {
     $(".all-stories").append(createStories(story)); //append all the stories
   }
 };
+const renderSingleStory = (stories) => {
+  $(".all-stories").empty();
+
+  for (story of stories) {
+    $(".all-stories").append(createStories(story)); //append all the stories
+  }
+};
 //create the story elements to append in the stories.ejs
 const createStories = (story) => {
   let snippet = "";
   for (let i = 0; i < 120; i++) {
     snippet += story.text[i];
   }
-  let $story = $(`<wired-card elevation="4" class="story">
+  let $story = $(`<wired-card elevation="4" id="story-${story.id}" class="story">
   <header>
     <span class="story-title">${story.title}</span>
   </header>
@@ -28,9 +35,37 @@ const createStories = (story) => {
     ${story.tags}
     </div>
   </footer>
-  </wired-card>"`);
+  </wired-card>`);
 
   return $story;
 };
 
-$(document).ready(loadStories(renderStories));
+const createSingleStory = (storyId) => {
+  let $story = $(`<wired-card elevation="4" id="story-${story.id}" class="story">
+  <header>
+    <span class="story-title">${story.title}</span>
+  </header>
+  <p class="story-text">${story.text}...</p>
+  <span class="story-read-more">Read More</span>
+  <footer class="story-tags">
+    ${story.tags}
+    </div>
+  </footer>
+  </wired-card>`);
+
+  return $story;
+};
+
+const renderSingleStory = () => {
+  let allWiredCards = $("wired-card");
+
+  $("main").on("click", () => {
+    console.log($(event.target).parent(".story")[0].id);
+  });
+};
+
+$(document).ready(() => {
+  renderSingleStory();
+  loadStories(renderStories);
+  createSingleStory();
+});
