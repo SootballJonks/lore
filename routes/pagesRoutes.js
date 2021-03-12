737a74f786ab7ccb4bdd73c33127eaae2ccc563f
zezking <zhaoenze001@gmail.com>
2021-03-11 18:46:49 -0800

    add user login msg

--- routes/pagesRoutes.js
+++ routes/pagesRoutes.js
@@ -6,12 +6,13 @@ const router = express.Router();
 //If logged in, it will render the homepage. If not, redirects to login. Only users can access the website.
 router.get("/", (req, res) => {
   const username = req.session.username;
-
+  console.log(username);
   if (!username) {
     res.redirect("/login");
   }
   if (username) {
-    res.render("index"); //index is our homepage. stories.ejs is now obsolete
+    res.render("index", { username });
+    //index is our homepage. stories.ejs is now obsolete
   }
 });
 
