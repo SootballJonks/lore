e2066aa67e47331be514232f53e79e01c65df4a9
Jonks <anastasiablackhold@gmail.com>
2021-03-12 01:21:26 +0000

    Populated database with more content.

--- db/seeds/02_stories.sql
+++ db/seeds/02_stories.sql
@@ -56,9 +56,201 @@ INSERT INTO
 VALUES
   (
     1,
-    'TEST COMPLETED STORY',
-    'This is a completed story. It might not look it, due to its size, but rest assured this story is indeed completed. May the flames guide thee, unkindled one.',
-    ARRAY ['test'],
+    'At The End Of The Day',
+    '
+    He is sixteen and on his first hunt.
+    </br>
+    He''s panting, running back and forth to find the materials for the quest. He wants very much to make a good first impression on the Guildmarm and the rest of the Caravan.
+    </br>
+    At the end of the day, he and his Palico high five after a successful quest.
+    </br></br>
+    The twenty sixth day of the ninth month.
+    </br>
+    He is sixteen and is still learning the ropes.
+    </br>
+    Not even twenty days into the business, and he''s already sick of the older hunters calling him "cheap pickle bait". How the hell do you bait a pickle, anyway? Are they even sentient? As he''s grumbling, the cliffs above him tremble. Two large beasts above him locked in combat. By the time it dies down and he steps out from behind the rocks, he sees an egg.
+    </br>
+    At the end of the day, he''s made a space for the egg in his cabin.
+    </br></br>
+    The twenty eighth day of the ninth month.
+    </br>
+    He is sixteen and is sleeping peacefully until his Palico shakes him awake.
+    </br>
+    He is angry at the loss of sleep, before they both hear loud cracking noises, and the Palico tells him angrily that heat has a tendency to make monster eggs hatch faster.
+    </br>
+    At the end of the day, a dragon covered in spikes is born, and the very first thing it does upon breaking it''s shell is bite down on his hand.
+    </br></br>
+    The seventeenth day of the fourth month.
+    </br>
+    He is twenty eight and is roasting meat on a spit.
+    </br>
+    He is covered in scars and bite marks, very few of them are from the monsters he hunts. Many more of them are from the monster he raises. The aforementioned monster has grown large enough that they''ve relocated into a large cave.
+    </br>
+    At the end of the day, he finished cooking dinner for the dragon, and to his surprise, it takes the meal without stealing any of his.
+    </br></br>
+    The first day of the second month.
+    </br>
+    He is fifty two and is defending a caravan from what looks to be a large boar covered in red slime.
+    </br>
+    His techniques with the long sword are still formidable, but he is not as young nor as strong as he once was. It is a matter of time before one of the boar''s large horns is planted in his leg. His vision blurs as he flies through the air and lands on the ground. His palico hurries over with medicine, and his eyes flutter closed.
+    </br>
+    At the end of the day, he hears a horrid shriek, and opens his eyes to see the dragon tearing the boar to shreds.
+    </br></br>
+    The sun is in the middle of the sky in the Cold Season.
+    </br>
+    She is nearing four hundred and fifty, and she is aware of the uniformed humans watching her and documenting her movements.
+    </br>
+    She has patrolled this cave for a long, long time, more than their pitiful human minds could comprehend. Few dare approach her, even her own species trembles at the sound of her roar, a vicious barbarian among the other Elder Dragons. One of her eyes is put out, an emblem of pride from when she hunted down his killer and planted her claws in it''s head.
+    </br>
+    At the back of the cave, a long sword is buried in the rock. An odd looking catlike helmet rests on top of it. A small cabin has rotted into a pile of old wood and tools that have long broken.
+    </br>
+    At the end of the day, it is another day that she has kept her home safe, another day that she has kept their spirits safe. She will keep it that way.',
+    ARRAY ['monsterhunter, monster, hunter, nergigante'],
     false
   );
 
+INSERT INTO
+  stories (user_id, title, text, tags)
+VALUES
+  (
+    3,
+    'Adoption by Defenestration',
+    '"Brat duty" is every ANBU’s least favorite job.
+    </br></br>
+    No matter what an outsider might think, if outsiders were given access not only to ANBU mission orders but also their complicated bartering system to get out of such missions, it has nothing to do with the brat being the jinchuuriki. The thing’s sealed away, and the brat’s basically a baby; the brat’s no more a demon than a summoning scroll is the animal it summons.
+    </br></br>
+    No, they hate brat duty because it’s literally babysitting, except they don’t even get to interact with the baby. Watching a child crawl in circles and cry for hours while sitting on a tree branch is fucking boring, and no matter how good they are at surveillance, ANBU do not take boredom well.
+    </br></br>
+    No one wants to outright refuse the mission--it’s Yondaime-sama’s kid, for fuck’s sake--but no one actually wants to do it, either. Hence the complicated system of bartering and trading favors to avoid taking on the mission for as long as possible while still maintaining the recognition of the theoretical honor of the mission.
+    </br></br>
+    But, inevitably, someone has to do it, which is how Tenzo finds himself in a tree outside of the brat’s apartment--because Sandaime-sama, despite all his wisdom, decided that leaving the brat alone was the best possible choice, and Tenzo doesn’t like to question orders but he really wants to question this one--watching the kid try to climb out his own window.
+    </br></br>
+    Tenzo isn’t actually sure the kid realizes it’s a window, but in the end what matters isn’t what the kid knows but the fact that he manages to climb up to the window ledge, shove the window open, and then slip and tip himself out of the apartment. Tenzo catches him before he can fall, but then he’s holding the fucking child and, without clear orders of what to do with actual physical contact with the kid, goes back to the tree for lack of other options.
+    </br></br>
+    The kid is screaming in his ear, and he realizes after a couple seconds that it’s in excitement rather than fear, because the kid is a fucking moron.
+    </br></br>
+    “Stop,” Tenzo says, because he would really like the kid not to be screaming in his ear.
+    </br></br>
+    The kid’s screaming cuts off, thank fuck, and then he peers up at Tenzo with wide blue eyes. “Again?” the kid asks plaintively.
+    </br></br>
+    Tenzo blinks. “I’m not going to let you try to climb out your window again just so I can catch you.”
+    </br></br>
+    “Again?” The kid starts bouncing in his arms. “Again, again, again !” He’s shrieking now, which isn’t any better than the screaming, and Tenzo wonders with some desperation if any other ANBU have had to actually deal with the kid before. None of them reported it.
+    </br></br>
+    “I’m going to put you back in your house,” Tenzo tells him. “Don’t climb out the window again.”
+    </br></br>
+    He goes back to the apartment, sticks the kid on the floor, and heads back out the window, shutting it behind him. He goes back to the tree. The kid stares at him through the window, bottom lip sticking out, then goes determinedly back to trying to escape his apartment via his window.',
+    ARRAY ['Naruto, Tenzou, fanfiction, humor']
+  );
+
+INSERT INTO
+  stories (user_id, title, text, tags)
+VALUES
+  (
+    7,
+    'My Immortal',
+    '  Hi my name is Ebony Dark’ness Dementia Raven Way and I have long ebony black hair (that’s how I got my name) with purple streaks and red tips that reaches my mid-back and icy blue eyes like limpid tears and a lot of people tell me I look like Amy Lee (AN: if u don’t know who she is get da hell out of here!). [[I’m not related to Gerard Way but I wish I was because he’s a major fucking hottie. I’m a vampire but my teeth are straight and white. I have pale white skin. I’m also a witch, and I go to a magic school called Hogwarts in England where I’m in the seventh year (I’m seventeen). I’m a goth (in case you couldn’t tell) and I wear mostly black. I love Hot Topic and I buy all my clothes from there. For example today I was wearing a black corset with matching lace around it and a black leather miniskirt, pink fishnets and black combat boots. I was wearing black lipstick, white foundation, black eyeliner and red eye shadow. I was walking outside Hogwarts. It was snowing and raining so there was no sun, which I was very happy about. A lot of preps stared at me. I put up my middle finger at them.
+    </br></br>
+    “Hey Ebony!” shouted a voice. I looked up. It was…. Draco Malfoy!
+    </br></br>
+    “What’s up Draco?” I asked.
+    </br></br>
+    “Nothing.” he said shyly.
+    </br></br>
+    But then, I heard my friends call me and I had to go away.',
+    ARRAY ['cursed, seriouslyThisIsCursed, fanfiction, harryPotter']
+  );
+
+INSERT INTO
+  stories (user_id, title, text, tags, active)
+VALUES
+  (
+    6,
+    'A Kylo Ren/Rey fanfic',
+    'Rey picks up Kylo and dunks him into the garbage. The end.',
+    ARRAY ['fanfiction, starwars, bestseller'],
+    false
+  );
+
+INSERT INTO
+  stories (user_id, title, text, tags)
+VALUES
+  (
+    4,
+    'The Continual State of Being Fine (or how to live in Denial)',
+    'It came as a bit of a surprise when Doctor Halsey contacted Fred, who was checking the last of the supply list on the Autumn, and requested for him to go to the infirmary to meet the Master Chief.
+    </br></br>
+    “Is he alright?” Fred asked, passing off the list to Kelly. Years of working together allowed her to know what he wanted her to do without him having to say a word.
+    </br></br>
+    “It’s the Master Chief,” Halsey replied, giving Fred no real answer. “Please assist him in whatever way he may need. I will be in my lab doing some last minute repairs before the Autumn disembarks and cannot be disturbed again.”
+    </br></br>
+    He wondered what she meant by “again”, but he knew that his inquisitiveness would not be appreciated by the doctor. “Understood, ma’am.”
+    </br></br>
+    It took him less than twenty minutes to reach the facility and, not for the first time, he wondered what happened to John and why the Chief hadn’t contacted him directly. He frowned, having a problem so close to leaving Reach could jeopardize their new mission’s success. Before he could further consider the issue, a female voice cut through the air.
+    </br></br>
+    “Do you want to be the one who explains to Doctor Halsey why she has to mend your Achilles Heel again?” There was a loud sigh. “You know, she didn’t mention how stubborn you were. Though if she had, I might have reconsidered my choice.”
+    </br></br>
+    The unfamiliar voice floated down the hall, causing Fred to stop outside room 219. This was the room that Halsey had instructed him to go to, but he didn’t recognize the woman speaking on the other side of the door. What choice was she talking about?
+    </br></br>
+    John’s voice cut through the air. Fred could detect a layer of frustration, though he could tell that the Chief was doing his best to mask it. “I’m fine, Cortana.”
+    </br></br>
+    Fred’s brows furrowed. Who was Cortana?
+    </br></br>
+    He pressed the access panel and the door to the exam room slid open. Inside, John was sitting with his legs over the side of the bed. His armor was nowhere to be found, which could explain why he hadn’t contacted Fred directly; instead, the Spartan was wearing loose fitting pants and a t-shirt. The smell of antiseptics and biofoam told Fred the story faster than John could: he had been somehow seriously injured, despite never leaving the base that morning.
+    </br></br>
+    By the side of the bed, projecting from a plinth, was an unfamiliar AI with an extremely annoyed look. She paid no attention to Fred and crossed her arms as she said, “Fine? We almost died out there and you’re ‘fine’? Do I need to tell you the extensive list of injuries you sustained out there? Spartan or not, you need time to recover, Chief. Those were Doctor Halsey’s orders too, if you remember correctly.”',
+    ARRAY ['halo, master, chief, cortana']
+  );
+
+INSERT INTO
+  stories (user_id, title, text, tags, active)
+VALUES
+  (
+    8,
+    'Love Found In A Box',
+    'Jaina frowned, looking around the camp.
+    </br></br>
+    She wanted to have a talk with the Ranger General, but for the love of Tides, she couldn’t find her anywhere!
+    </br></br>
+    She stopped one of the rangers that were mulling around the camp, “I’m sorry, but have you seen Ranger General?” The man just shook his head no, apologizing. Jaina smiled and thanked him, shuffling off.
+    </br></br>
+    Jaina somehow wandered to the back of the camp, where the majority of crates and sacks of supplies were stored.
+    </br></br>
+    Her thoughts were flying around in her head as she tried to see if her suggestions and potential offers would be accepted and were fair for both sides when something caught her eye.
+    </br></br>
+    She stopped, starring at a top of a head and points of long ears, sticking out of an open wooden box. As well as legs, hanging over the edge. With a frown, Jaina slowly made her way over, carefully looking inside.
+    </br></br>
+    To her surprise, she’d found the oh-so-elusive Ranger General Sylvanas Windrunner, snoozing away, hidden from prying eyes… in a large wooden box.
+    </br></br>
+    Clapping a hand over her mouth, Jaina barely managed to hold in a laugh that threatened to escape her. She stood there for a moment, committing that image to memory. With a deep breath, her lips spread in a wide smile, she cleared her throat.
+    </br></br>
+    “General?” All she got in reply was a disgruntled grumble. She giggled quietly, repeating again, louder now. “General.”
+    </br></br>
+    A glowing blue eye opened lazily, looking up at her. There was a moment of silence and then the situation seemed to register in Sylvanas’ brain as both her eyes went wide and she scrambled out, tripping all over the place, her foot catching the edge of the box, tripping her again. Gone was all the gracefulness that Jaina had seen in the last couple of weeks; along with the easy confidence that seemed to be a permanent part of the elf. Her cheeks were burning red and her ears pressed in embarrassment against her skull as she fixed her clothes, clearing her throat.
+    </br></br>
+    “Lady P-proudmoore.” Jaina giggled again, louder, noting with little victory when the blush spread all the way along Sylvanas’ ears. Now she''d taste her own medicine, for it''d been Jaina, who was a constant blushing, blubbering mess around the elf. “What can I do for you?”
+    </br></br>
+    “I’m sorry I’ve disturbed your nap, General.” Jaina spoke, trying not to giggle. “I was hoping to have a few words with you about the trading agreements.”
+    </br></br>
+    “Of course.” Sylvanas cleared her throat again, her face still flaring with embarrassment, as she tugged her cuirass down one last time, extending her arm out, motioning for Jaina to lead the way. “After you, my lady.”
+    </br></br>
+    “The first time I’ve seen you,” Jaina''s voice was shaking with emotions and tears were clouding her vision, her cheeks hurting from how much she was smiling. The hands that were holding her own, gave a reassuring squeeze. “I never thought that I would be lucky enough to know you as a friend, let alone a lover.” She met eyes with Sylvanas, who was standing before her, holding Jaina''s hands, her own glowing eyes misted with tears of happiness. With a mischievous glint and smirk, Jaina continued. “I thought someone like you would never want to do anything with a simple mage as myself. Especially after I’ve found you taking a nap in that box at the back of the camp.”
+    </br></br>
+    There was a moment of silence.
+    </br></br>
+    First the air was torn by a loud bark of a laugh that soon turned into a full-bellied one as Alleria practically bent in two where she was standing behind Sylvanas, arms around her midriff. The crowed of guests joined next.  The air ringing with happy free laughter and giggles.
+    </br></br>
+    Jaina saw from the corner of her eyes as Lor’themar was trying to hold his laughter in and failing, pressing a palm over his eyes and chuckling.
+    </br></br>
+    She looked at Sylvanas, smiling widely when she saw the same full-faced blush and the pinned ears she’d seen all those years ago at the back of that camp.
+    </br></br>
+    “I should have known you’d slip that in somewhere.” Sylvanas grumbled. “But in your vows?!”
+    </br></br>
+    “Forgive me, my love,” Jaina managed through her own happy laughter, “but I’ve been keeping this secret all to myself for too long. People deserve to know.”
+    </br></br>
+    “You’re lucky I love you.”
+    </br></br>
+    “I would hope so. We are in the middle of our wedding.”',
+    ARRAY ['warcraft, WoW, jaina, sylvanas'],
+    false
+  );
